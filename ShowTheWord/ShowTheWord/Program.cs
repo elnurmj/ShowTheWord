09b159766eab1e95dec9d9101f63c955800cbd0d
unknown <elnurmj@code.edu.az>
2021-12-07 21:17:22 +0400

    Show the Word

---
+++ ShowTheWord/ShowTheWord/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShowTheWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Hey Elnur.How are you Elnur";
+            Console.WriteLine(sentence.ShowWord("Elnur"));
+        }
+    }
+}
---
+++ ShowTheWord/ShowTheWord/ShowTheWord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ShowTheWord
+{
+    public static class ShowTheWord
+    {
+        public static int ShowWord(this string str, string pattern)
+        {
+            return Regex.Matches(str, pattern).Count;
+        }
+
+    }
+}
